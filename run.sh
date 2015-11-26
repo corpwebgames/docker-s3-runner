0f2bd8a11a0ce0dc7c6f3558d1f6742ee713afcc
EC2 Default User <ec2-user@ip-172-31-26-86.ec2.internal>
2015-11-26 14:59:32 +0000

    add sns notify result

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+NEWLINE=$'\n'
 
 aws s3 cp $1 s3.sh
 chmod +x s3.sh
 
-./s3.sh $ARGS
+output=$(./s3.sh $ARGS)
+status=$?
+
+export AWS_DEFAULT_REGION=us-east-1
+aws sns publish --topic-arn "$ARN" --subject "Task: $ID" --message "Status: $status ${NEWLINE}${NEWLINE}Output: $output"
