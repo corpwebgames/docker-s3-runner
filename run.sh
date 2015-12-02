6ae025c52b84673f5034eedd17c1de13ea2d88b4
EC2 Default User <ec2-user@ip-172-31-26-86.ec2.internal>
2015-12-02 13:33:38 +0000

    json output format

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-NEWLINE=$'\n'
+#NEWLINE=$'\n'
 
 aws s3 cp $1 s3.sh
 chmod +x s3.sh
 
-output=$(./s3.sh $ARGS)
-status=$?
+export output=$(./s3.sh $ARGS)
+export status=$?
+export id=$ID
+
+#message="{\"id\":\""$ID"\", \"status\":\""$status"\", \"output\":\""$output"\"}"
+
+message=`python <<END
+import sys, os, json
+
+data = {}
+data['id'] = os.environ['id']
+data['status'] = os.environ['status']
+data['output'] = os.environ['output']
+json_data = json.dumps(data)
+
+print json.dumps(data)
+
+END`
 
 export AWS_DEFAULT_REGION=us-east-1
-aws sns publish --topic-arn "$ARN" --subject "Task: $ID" --message "Status: $status ${NEWLINE}${NEWLINE}Output: $output"
+aws sns publish --topic-arn "$ARN" --subject "Task: $ID" --message "$message"
